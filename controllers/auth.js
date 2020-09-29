0cbf319e077cdfe20277b47e5229dc356bd6108a
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-29 17:46:12 -0500

    queried to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
-
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  datebase: process.env.DATABASE,
+});
 
 exports.register =(req, res) => {
 console.log(req.body);
+
+const { name, email, password, passwordConfirm} = req.body;
+
+db.query()
+
 res.send("Form Submitted");
 }
