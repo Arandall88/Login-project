13011a646c09842d8d059af06d1a83dcd7f2ae43
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-29 12:56:33 -0500

    created mysql database created  connection to database

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password:'',
+    datebase:'login-nodejs'
+});
+
+db.connect( (error)=>{
+if(error) {
+    console.log(error)
+} else {
+    console.log("MYSQL Connected...")
+}
+})
 
 app.get("/", (req,res) => {
     res.send("<h1>Home Page</h1>")
