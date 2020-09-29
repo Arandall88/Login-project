6a86de7923299044dde407b8ff6fe81395da1998
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-29 18:01:14 -0500

    added bcrypt install cookie-parser and json web token logged errors and return messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
@@ -11,7 +12,20 @@ console.log(req.body);
 
 const { name, email, password, passwordConfirm} = req.body;
 
-db.query()
+db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {
+    if(error) {
+        console.log(error);
+    } 
+    if(results.length > 0) {
+        return res.render('register', {
+            message: 'That email is already in use'
+        })
+    } else if ( password !== passwordConfirm ) {
+        return res.render('register', {
+            message: 'Passwords do not match'
+    });
+}
 
 res.send("Form Submitted");
+})
 }
