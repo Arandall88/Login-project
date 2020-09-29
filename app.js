29b71f27cc501db32bfa319bd8999e29e78bada9
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-29 15:35:28 -0500

    put all sensitive info in dotenv

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env'});
 
 const app = express();
 
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password:'',
-    datebase:'login-nodejs'
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  datebase: process.env.DATABASE,
 });
+
+app.set('view engine', 'hbs');
 
 db.connect( (error)=>{
 if(error) {
